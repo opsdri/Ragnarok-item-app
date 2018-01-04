0cc8a4ce8499786ceea27c497ff6fca8dc0350ac
John Aldrie <zspudsz@gmail.com>
2018-01-04 21:09:54 +0400

    no message

--- RomItems/ViewController.swift
+++ RomItems/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-       
+    
+    
+            }
+    
     }
-  
-}
