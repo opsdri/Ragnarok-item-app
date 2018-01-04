9360fc354830a910052c3c86aff5da42d9591b5f
John Aldrie <zspudsz@gmail.com>
2018-01-04 21:57:47 +0400

    finished error in UItableViewdatasource

--- RomItems/ViewController.swift
+++ RomItems/ViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var games : [Rom] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     
-    
-            }
     
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+        games = try context.fetch(Rom.fetchRequest())
+        } catch {
+        
+        }
+    
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let game = games[indexPath.row]
+        cell.textLabel?.text = game.title
+        return cell
+    }
+}
